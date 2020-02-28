d60182629483315e38b76e9570dfee3a8ef53717
Dane-Dawson <Dane.P.Dawson@gmail.com>
2020-02-28 12:31:17 -0600

    last time hopefully

--- app/models/power.rb
+++ app/models/power.rb
@@ -1,6 +1,6 @@
 class Power < ApplicationRecord
     belongs_to :heroinepowers
-    belongs_to :heroines, through: :heroinepowers
+    #belongs_to :heroines, through: :heroine_powers
 
     def heroine_count
         Power.heroines.count 
