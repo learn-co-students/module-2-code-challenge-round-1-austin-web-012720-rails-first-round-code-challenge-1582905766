d7febba3c20fc13372259cf678d93ec18207e740
github username <github email address>
2020-02-28 12:17:36 -0600

    Maylene's Mod2 Code Challenge

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,28 @@
 class PowersController < ApplicationController
+  before_action :find_power, only: [:show, :edit, :update]
   def index
     @powers = Power.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    
+    if @power.valid?
+      @power.update
+      redirect_to power_path(@power)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def find_power
+    @power = Power.find(params[:id])
   end
 end
