32ae5c83c28407cd596935d4016c0bcaf431399b
franco-canedo <francanedo1@hotmail.com>
2020-02-28 12:16:02 -0600

    finish

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,29 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show 
+    @power = Power.find(params[:id])
+  end 
+
+  def edit
+    @power = Power.find(params[:id])
+  end 
+
+  def update
+    @power = Power.find(params[:id])
+    
+    
+     HeroinePower.find_or_create_by(heroine_id: params[:heroine_ids], power_id: params[:id]) 
+    # binding.pry
+    @power.update(power_params)
+    redirect_to power_path(@power)
+
+  end 
+
+  private
+
+  def power_params
+    params.require(:power).permit(:name, :description, :heroine_ids)
+  end 
 end
