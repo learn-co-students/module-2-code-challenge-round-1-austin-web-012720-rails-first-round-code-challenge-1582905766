d8c217ea630f94c334cced66721c15a771bb08ae
github username <github email address>
2020-02-28 12:31:23 -0600

    my code challenge

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,25 @@
 class HeroinesController < ApplicationController
+
   def index
     @heroines = Heroine.all
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new 
+  end
+
+  def create
+    @heroine = Heroine.new(params.require(:heroine).permit(:name, :super_name))
+    if @heroine.valid?
+      @heroine.save
+      redirect_to heroine_path(@heroine)
+    else 
+      flash[:errors] = @heroine.errors.full_messages
+      render "new"
+    end
   end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,26 @@
 class PowersController < ApplicationController
+
   def index
     @powers = Power.all
+  end
+
+  def show
+    @power = Power.find(params[:id])
+  end
+
+  def new
+    @power = Power.new
+  end
+
+  def create
+    @power = Power.new(params.require(:power).permit(:name, :description))
+    @power.save
+    redirect_to power_path(@power)
+  end
+
+  def destroy
+    @power = Power.find(params[:id])
+    @power.destroy
+    redirect_to powers_path
   end
 end
