50fdf03a44cabdea33bce0ceeb57099dcf7a5bde
lughjw <lughjw@gmail.com>
2020-02-28 12:15:37 -0600

    Done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,36 @@
 class HeroinesController < ApplicationController
+  before_action :set_heroine, only: [:show]
+  
   def index
     @heroines = Heroine.all
+  end
+
+  def show
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+
+    if @heroine.valid?
+      @heroine.save
+      redirect_to @heroine
+    else
+      flash[:messages] = @heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+
+  end
+
+  private
+  def set_heroine
+    @heroine = Heroine.find_by(id: params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power)
   end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,39 @@
 class PowersController < ApplicationController
+  before_action :set_power, only: [:show, :edit, :update]
+  
   def index
     @powers = Power.all
+  end
+
+  def show
+  end
+  
+  def edit
+    @heroines = Heroine.all
+  end
+
+  def update
+    @power.update(power_params)
+    params[:power][:heroines].each do |heroine_id|
+      @power.heroines << Heroine.find_by(id: heroine_id.to_i)
+    end
+    # binding.pry
+    if @power.valid?
+      @power.save
+      redirect_to @power
+    else
+      flash[:messages] = @power.errors.full_messages
+      redirect_to edit_power_path
+    end
+  end
+
+
+  private
+  def set_power
+    @power = Power.find_by(id: params[:id])
+  end
+
+  def power_params
+    params.require(:power).permit(:name, :description, :heroines)
   end
 end
